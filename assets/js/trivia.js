e6ccac62c8af4b2467af7cd7a5c95837c78b478e
Lino Suarez <linosuarez@Linos-MacBook-Pro.local>
2017-11-02 12:38:14 -0700

    Questions added

---
+++ assets/js/trivia.js
@@ -0,0 +1,14 @@
+var questions = { //QUESTION, 3 possible answers
+    0: ["In a photo editing program, what do the letters RGB stand for?", "Red, Green & Blue", "Roll, Gorila, Baby", "Rolling, Gorillas, Bowling"],
+    1: ["The Commonwealth of the Bahamas gained independence in 1973 from what country?", "Spain","United Kingdom", "Grait Britain"],
+    2: ["Who painted the Sistine Chapel?", "Michelangelo", "Leonardo da Vinci", "Lorenzo Ghiberti"],
+    3: ["American mobster Al Capone was sentenced to 11 years in federal prison for what crime?", "Murder", "Rape", "Tax Evasion"]
+}
+var answers = { //Question & answer Index
+    0: [1],
+    1: [2],
+    2: [1],
+    3: [3]
+}
+
+
